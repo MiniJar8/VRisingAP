5aff7e5bbd6c95619032ac45fab1f47a21a041d1
Abbi <irreversiblyabbi8@gmail.com>
2026-07-04 15:05:37 -0500

    Flake

---
+++ Client/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+
+namespace Client;
+
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin
+{
+    Harmony _harmony;
+
+    public override void Load()
+    {
+        // Plugin startup logic
+        Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} version {MyPluginInfo.PLUGIN_VERSION} is loaded!");
+
+        // Harmony patching
+        _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
+        _harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+
+    }
+
+    public override bool Unload()
+    {
+        _harmony?.UnpatchSelf();
+        return true;
+    }
+
+}
--- flake.nix
+++ flake.nix
@@ -1,14 +1,26 @@
 {
   description = "A Nix-flake-based C# development environment (tailored for V Rising mod dev)"; #Stolen from: https://github.com/NorseManGef/csharptemplate/blob/master/flake.nix
 
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  inputs= {
+    nixpkgs = { 
+        url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+        };
+    };
 
   outputs = { self, nixpkgs }:
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
-      });
+        pkgs = import nixpkgs { 
+            inherit system; 
+            config.allowUnfree = true; 
+            config.permittedInsecurePackages = [
+                "dotnet-sdk-6.0.428"
+                "dotnet-runtime-6.0.36"
+            ];
+            };
+        }
+      );
     in
     {
       devShells = forEachSupportedSystem ({ pkgs }: {
@@ -21,7 +33,11 @@
             packages = with pkgs; [
               dotnet-sdk_6
               dotnet-runtime_6
+              python3
             ];
+            shellHook = ''
+              export PS1="\n\[\033[38;2;255;0;0m\][V Rising]\[\e[0m\] \[\033[1;32m\][\[\e]0;\u@\h: \w\a\]\u@\h:\w]\$\[\033[0m\] "
+            ''; #Make red V Rising text appear when in dev environment. Change color with: https://plinkon.github.io/Colorify/
           };
       });
     };
